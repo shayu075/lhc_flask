4a3c493ed4d09a050e0d34928c3dd3d0c3fb5f52
shayu075 <454452063@qq.com>
2019-01-05 01:02:15 +0800

    去掉跨域

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from common.service import get_list_same_num_by_type
 app = Flask(__name__)
 
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,session_id')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,HEAD')
-    # 这里不能使用add方法，否则会出现 The 'Access-Control-Allow-Origin' header contains multiple values 的问题
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
+# @app.after_request
+# def after_request(response):
+#     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,session_id')
+#     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,HEAD')
+#     # 这里不能使用add方法，否则会出现 The 'Access-Control-Allow-Origin' header contains multiple values 的问题
+#     response.headers['Access-Control-Allow-Origin'] = '*'
+#     return response
 
 
 @app.route('/lhc_flask/ps', methods=['GET'])
